a517d4eb368377d22ab37e57af7bb3ddc07499d3
Endy <endymion1@vp.pl>
2020-10-22 19:46:21 +0200

    first commit

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace KomunikatorTekstowy.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KomunikatorTekstowy.WebApiApplication" Language="C#" %>
