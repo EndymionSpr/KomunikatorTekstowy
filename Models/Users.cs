a33838788442e7d674c3f1dfed7adfc719a76ff4
Endy <endymion1@vp.pl>
2020-10-26 18:19:39 +0100

    Klasa DB Users i wypełnianie bazy.

---
+++ Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace KomunikatorTekstowy.Models
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Key]
+        [Column("id")]
+
+        public int Id { get; set; }
+        [Required]
+        [Column("nickname")]
+
+        public string Nickname { get; set; }
+
+        [Required]
+        [Column("password")]
+
+        public string Password { get; set; }
+
+    }
+}
