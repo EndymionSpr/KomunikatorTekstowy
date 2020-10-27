6141c334891cd189c786f27a459a004489a95f62
Endy <endymion1@vp.pl>
2020-10-27 13:18:35 +0100

    Restart, tym razem .NET Core. Db i Users.cs dodane

--- Database/IntroductionDbContext.cs
+++ Database/IntroductionDbContext.cs
@@ -4,37 +4,40 @@ using Microsoft.EntityFrameworkCore;
 
 namespace KomunikatorTekstowy.Database
 {
-    public class IntroductionDbContext: DbContext    
+    public class IntroductionDbContext : DbContext
     {
         public IntroductionDbContext(DbContextOptions options) : base(options) { }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Users>().HasData(new Users() 
-        { 
-            Id = 1, 
-            Nickname = "Task #1" 
-            , Password = "qweqwe"
-        }, 
-        new Users()
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            Id = 2,
-            Nickname = "Task #2"
-            ,
-            Password = "qweqwe"
-        }, 
-        new Users() 
-        { Id = 3, 
-            Nickname = "Task #3"
-            ,
-            Password = "qweqwe"
-        }, 
-        new Users() 
-        { Id = 4, 
-          Nickname = "Task #4"
-          ,
-            Password = "qweqwe"
-        });
+            modelBuilder.Entity<Users>().HasData(new Users()
+            {
+                Id = 1,
+                Nickname = "Task #1"
+                ,
+                Password = "qweqwe"
+            },
+            new Users()
+            {
+                Id = 2,
+                Nickname = "Task #2"
+                ,
+                Password = "qweqwe"
+            },
+            new Users()
+            {
+                Id = 3,
+                Nickname = "Task #3"
+                ,
+                Password = "qweqwe"
+            },
+            new Users()
+            {
+                Id = 4,
+                Nickname = "Task #4"
+              ,
+                Password = "qweqwe"
+            });
+        }
+        public DbSet<Users> Users { get; set; }
     }
-    public DbSet<Users> Users { get; set; }
-}
 }
