e660f505969dea7e89add2167a67b31c704afad0
Endy <endymion1@vp.pl>
2020-10-28 16:13:14 +0100

    Dodanie connection stringa do json'a. Zrobienie pierwszej migracji.

---
+++ Migrations/20201028150945_InitialMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace KomunikatorTekstowy.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    nickname = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "id", "nickname", "Password" },
+                values: new object[,]
+                {
+                    { 1, "User1", "qweqwe" },
+                    { 2, "User2", "qweqwe" },
+                    { 3, "User3", "qweqwe" },
+                    { 4, "User4", "qweqwe" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
